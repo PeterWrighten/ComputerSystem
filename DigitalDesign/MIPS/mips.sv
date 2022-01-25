9b716ad9b37a4289e8d7c4a909016783d6ad34c9
PeterWright <peterwrighten@gmail.com>
2022-01-25 21:55:09 +0900

    update::mips::controller

---
+++ DigitalDesign/MIPS/mips.sv
@@ -0,0 +1,19 @@
+module mips (input logic         clk, reset,
+             output logic[31:0]  pc,
+             input logic[31:0]   instr,
+             output logic        memwrite,
+             output logic[31:0]  aluout, writedata,
+             input logic[31:0]   readdata);
+
+    logic memtoreeg, alusrc, regdst,
+          regwrite, jump, pcsrc, zero;
+    logic[2:0] alucontrol;
+    controller c(instr[31:26], instr[5:0], zero, 
+                 memtoreg, memwrite, pcsrc,
+                 alusrc, regdst, regwrite, jump,
+                 alucontrol);
+    datapath dp(clk, reset, memtoreg, pcsrc, 
+                alusrc, regdst, regwrite, jump,
+                alucontrol, zero, oc, instr,
+                aluout, writedata, readdata);
+endmodule
